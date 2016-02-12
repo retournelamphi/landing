1549dfdb43a58bee4b519bfdc249e4d64983b5c0
Jérémy Morin <hi@jermor.in>
2016-02-12 18:46:36 +0100

    refacto(subscription) use constant and promises

---
+++ server/constants/subscription.js
@@ -0,0 +1,8 @@
+export const MSG_ERROR_MAIL = 'Notre serveur mail rencontre des problèmes, merci de réessayer plus tard.';
+export const MSG_ERROR_MONGO_EMAIL_DUPLICATE = 'Ce mail est déjà utilisé.';
+export const MSG_ERROR_MONGO_ERROR = 'Un problème est survenu !';
+export const MSG_SUCCESS = 'Merci pour votre contribution !';
+
+export const MAIL_SERVER_ADRESS = 'hi@retournelamphi.fr';
+export const MAIL_SUBJECT = 'Retourne Lamphi subscription';
+export const MAIL_BODY = 'Toute la communauté vous remercie !';
--- server/controllers/subscription.js
+++ server/controllers/subscription.js
@@ -1,16 +1,39 @@
 import Subscription from '../models/subscription';
 import {sendgridKey} from './../private/sendgrid';
+import {
+    MSG_ERROR_MAIL,
+    MAIL_BODY,
+    MAIL_SUBJECT,
+    MAIL_SERVER_ADRESS,
+    MSG_ERROR_MONGO_EMAIL_DUPLICATE,
+    MSG_ERROR_MONGO_ERROR, MSG_SUCCESS
+} from './../constants/subscription'
+
 const sendgrid = require('sendgrid')(sendgridKey);
 
-const MSG_ERROR_MAIL = 'Notre serveur mail rencontre des problèmes, merci de réessayer plus tard.';
-const MSG_ERROR_MONGO_EMAIL_DUPLICATE = 'Ce mail est déjà utilisé.';
-const MSG_ERROR_MONGO_ERROR = 'Un problème est survenu !';
-const MSG_SUCCESS = 'Merci pour votre contribution !';
 
 const renderWithFlagMsg = (res, next, err = null, msg = null) => {
     res.render('home', {err, msg});
     next();
 };
+
+const sendMail = (email) => {
+    return new Promise((resolve, reject) => {
+        sendgrid.send(new sendgrid.Email({
+            to: email,
+            from: MAIL_SERVER_ADRESS,
+            subject: MAIL_SUBJECT,
+            text: MAIL_BODY
+        }), (err, json) => {
+            if (err) {
+                reject(MSG_ERROR_MAIL)
+            } else {
+                resolve(json)
+            }
+        });
+    });
+};
+
 
 export function subscribe(req, res, next) {
     const email = req.body.email;
@@ -21,27 +44,17 @@ export function subscribe(req, res, next) {
         date: new Date()
     });
 
-    subscription.save((errMongo, doc) => {
-        console.log(errMongo);
-        if (!errMongo) {
-            sendgrid.send(new sendgrid.Email({
-                to: email,
-                from: 'hi@retournelamphi.fr',
-                subject: 'Merci',
-                text: 'Merci pour cette subscription'
-            }), (err, json) => {
-                if (err) {
-                    renderWithFlagMsg(res, next, MSG_ERROR_MAIL);
-                } else {
-                    renderWithFlagMsg(res, next, null, MSG_SUCCESS);
-                }
-            });
+    subscription.save().then(λ => {
+        sendMail(email).then(λ => {
+            renderWithFlagMsg(res, next, null, MSG_SUCCESS);
+        }).catch(λ => {
+            renderWithFlagMsg(res, next, MSG_ERROR_MAIL);
+        })
+    }).catch((e) => {
+        if (e.code = 11000) {
+            renderWithFlagMsg(res, next, MSG_ERROR_MONGO_EMAIL_DUPLICATE);
         } else {
-            if (errMongo.code = 11000) {
-                renderWithFlagMsg(res, next, MSG_ERROR_MONGO_EMAIL_DUPLICATE);
-            } else {
-                renderWithFlagMsg(res, next, MSG_ERROR_MONGO_ERROR);
-            }
+            renderWithFlagMsg(res, next, MSG_ERROR_MONGO_ERROR);
         }
     });
 }

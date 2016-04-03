bdb39389e4e98b7ee567596a22a9d4dc1d87b4d2
Jérémy Morin <hi@jermor.in>
2016-04-03 18:50:50 +0200

    feat(countTo) count to directive

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -16,9 +16,6 @@ app.controller('MainCtrl', function ($http, toastr) {
             url: '/count'
         };
         $http(req).success(function (data, status, headers) {
-            if (!self.submitted) {
-                toastr.success('composée de ' + data + ' personnes t\'attend pour la révolution !', 'La communauté Retourne L\'amphi');
-            }
             self.count = data;
         });
     };
@@ -44,7 +41,6 @@ app.controller('MainCtrl', function ($http, toastr) {
             self.submitted = true;
             self.formSuccess = true;
             toastr.success(data, 'La communauté Retourne L\'amphi');
-            self.getCount();
         }).error(function (data, status, headers) {
             self.formError = true;
             self.submitted = false;
@@ -52,7 +48,67 @@ app.controller('MainCtrl', function ($http, toastr) {
             self.errorMsg = data;
         });
     };
-});
+}).directive('countTo', ['$timeout', function ($timeout) {
+        return {
+            replace: false,
+            scope: true,
+            link: function (scope, element, attrs) {
+
+                var e = element[0];
+                var num, refreshInterval, duration, steps, step, countTo, value, increment;
+
+                var calculate = function () {
+                    refreshInterval = 30;
+                    step = 0;
+                    scope.timoutId = null;
+                    countTo = parseInt(attrs.countTo) || 0;
+                    scope.value = parseInt(attrs.value, 10) || 0;
+                    duration = (parseFloat(attrs.duration) * 1000) || 0;
+
+                    steps = Math.ceil(duration / refreshInterval);
+                    increment = ((countTo - scope.value) / steps);
+                    num = scope.value;
+                }
+
+                var tick = function () {
+                    scope.timoutId = $timeout(function () {
+                        num += increment;
+                        step++;
+                        if (step >= steps) {
+                            $timeout.cancel(scope.timoutId);
+                            num = countTo;
+                            e.textContent = countTo;
+                        } else {
+                            e.textContent = Math.round(num);
+                            tick();
+                        }
+                    }, refreshInterval);
+
+                }
+
+                var start = function () {
+                    if (scope.timoutId) {
+                        $timeout.cancel(scope.timoutId);
+                    }
+                    calculate();
+                    tick();
+                }
+
+                attrs.$observe('countTo', function (val) {
+                    if (val) {
+                        start();
+                    }
+                });
+
+                attrs.$observe('value', function (val) {
+                    start();
+                });
+
+                return true;
+            }
+        }
+
+    }]);
 
 app.config(function(toastrConfig) {
     angular.extend(toastrConfig, {

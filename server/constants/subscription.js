cee4b974fc4bf1e0a437374a991a912e4b525f51
Jérémy Morin <hi@jermor.in>
2016-06-20 19:51:49 +0200

    fix(wording) mail body

--- server/constants/subscription.js
+++ server/constants/subscription.js
@@ -4,5 +4,15 @@ export const MSG_ERROR_MONGO_ERROR = 'Un problème est survenu !';
 export const MSG_SUCCESS = 'Te remercie pour ta contribution !';
 
 export const MAIL_SERVER_ADRESS = 'hi@retournelamphi.fr';
-export const MAIL_SUBJECT = 'Retourne Lamphi subscription';
-export const MAIL_BODY = 'Toute la communauté vous remercie !';
+export const MAIL_SUBJECT = 'Bienvenue dans la communauté Retourne l’Amphi !';
+export const MAIL_BODY = `La communauté Retourne l’Amphi est très heureuse de te compter parmi ses membres !
+
+Surveille bien ta boîte mail, tu recevras bientôt un questionnaire nous permettant de connaître ton opinion et ton/tes avis sur ton parcours universitaire.</b></p>
+
+Tu te poses des questions sur Retourne l’Amphi ?
+Envoie les nous à hi@retournelamphi.fr et nous y répondrons au plus vite.
+
+À très bientôt !
+
+L’équipe de Retourne l’Amphi.
+`;
